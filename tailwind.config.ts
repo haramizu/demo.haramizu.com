32560bb144616949cdb6a668080d07c9a0ef74a0
Shinichi Haramizu <shinichi@haramizu.jp>
2023-09-14 10:08:35 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ import { nextui } from "@nextui-org/react";
 
 const config = {
   content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
